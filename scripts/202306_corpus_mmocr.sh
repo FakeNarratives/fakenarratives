f987eb5aade45c5fba6ea95b8f692f174bdf1a10
muellerer <eric.mueller@tib.eu>
2023-06-30 12:22:03 +0200

    script to extract mmocr outputs for the fakenarratives corpus version 202306

---
+++ scripts/202306_corpus_mmocr.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+#SBATCH --nodes=1
+##SBATCH -w devbox2
+##SBATCH -s
+#SBATCH -J mmocr_fakenarratives
+#SBATCH -G 1    
+##SBATCH -c 24
+##SBATCH -o /nfs/home/muellerer/fakenarratives/slurm
+
+FPS=2
+NEWSCHANNELS=(BildTV CompactTV HeuteJournal Tagesschau)
+
+
+for c in $NEWSCHANNELS
+do
+    echo %%%% $c
+    videos=$(ls -d /nfs/data/fakenarratives/202306_corpus/videos/$c/*.mp4)
+    python ../pickle_mmocr_outputs.py --videos $videos --output /nfs/data/fakenarratives/202306_corpus/results_pkl/$c --fps $FPS
+done
