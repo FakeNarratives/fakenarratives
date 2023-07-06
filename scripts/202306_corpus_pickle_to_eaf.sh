2ef63d8a1ac4d473394822b9bdeba938178df407
muellerer <eric.mueller@tib.eu>
2023-07-06 21:56:11 +0200

    script to create eaf files for the corpus

---
+++ scripts/202306_corpus_pickle_to_eaf.sh
@@ -0,0 +1,19 @@
+NEWSCHANNELS=(BildTV CompactTV HeuteJournal Tagesschau)
+
+for c in ${NEWSCHANNELS[*]}
+do
+    echo %%%% $c
+    videos=$(ls /nfs/data/fakenarratives/202306_corpus/results_pkl/$c/)
+
+    for v in $videos
+    do
+        in_folder=/nfs/data/fakenarratives/202306_corpus/results_pkl/$c/$v
+        out_folder=/nfs/data/fakenarratives/202306_corpus/results_eaf_speakers/$c/$v
+
+        # echo $in_folder
+        # echo $out_folder
+        # echo %%%%
+
+        python pickle_to_eaf.py --input $in_folder --output $out_folder --speaker
+    done
+done
