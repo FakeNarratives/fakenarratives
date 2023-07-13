5c2fe9c28174877a553b5215c7018fe19c2827cb
gullalc <gullalcheema@gmail.com>
2023-07-13 17:05:59 +0200

    add modular functions

--- text_analysis/pickle_textnlp_outputs.py
+++ text_analysis/pickle_textnlp_outputs.py
@@ -66,9 +66,144 @@ def add_transcript_boundaries(asr_dict):    ## Segment boundaries are needed to 
 
     return asr_dict
 
+
+def get_sentiment(sen_model, asr_dict, proc_text):
+    # Sentence-wise sentiment
+    all_texts = [sent.text for sent in proc_text.sentences]
+    preds, probs = sen_model.predict_sentiment(all_texts, output_probabilities=True)
+    sent_sentiments = [{"id": str(i), "text": text, "pred": preds[i], "prob": [probs[i][0][1], probs[i][1][1], probs[i][2][1]]} for i, text in enumerate(all_texts)]
+
+    # Segment-wise sentiment
+    all_texts = [text_seg["text"].strip() for text_seg in asr_dict["output_data"]["segments"]]
+    preds, probs = sen_model.predict_sentiment(all_texts, output_probabilities=True)
+    seg_sentiments = [{"id": seg["id"], "seek": seg["seek"], "start": seg["start"], "end": seg["end"],
+                    "pred": preds[i], "prob": [probs[i][0][1], probs[i][1][1], probs[i][2][1]]} 
+                    for i, seg in enumerate(asr_dict["output_data"]["segments"])]
+
+    # Speaker-Segment-wise sentiment
+    all_texts = [text_seg["text"].strip() for text_seg in asr_dict["output_data"]["speaker_segments"]]
+    preds, probs = sen_model.predict_sentiment(all_texts, output_probabilities=True)
+    spk_sentiments = [{"id": str(i), "start": seg["start"], "end": seg["end"], "speaker": seg["speaker"],
+                    "pred": preds[i], "prob": [probs[i][0][1], probs[i][1][1], probs[i][2][1]]} 
+                    for i, seg in enumerate(asr_dict["output_data"]["speaker_segments"])]
+
+    return sent_sentiments, seg_sentiments, spk_sentiments
+
+
+def get_pos_tags(proc_text, proc_segments, proc_speaker_segments, asr_dict, pos_dict):
+    ## Sentence-wise POS Tags
+    sent_pos = []
+    for i, sent in enumerate(proc_text.sentences):
+        temp_pos = []
+        pos_vector = np.zeros(14)
+        for word in sent.words:
+            temp_pos.append((word.text, word.upos, word.xpos, word.start_char, word.end_char))
+            if word.upos in pos_dict:
+                pos_vector[pos_dict[word.upos]] += 1
+            else:
+                pos_vector[pos_dict['X']] += 1
+    
+        sent_pos.append({"id": str(i), "text": sent.text, "tags": temp_pos, "vector": pos_vector})
+
+    ## Segment-wise POS Tags
+    seg_pos = []
+    for seg_doc, seg in zip(proc_segments, asr_dict["output_data"]["segments"]):
+        temp_pos = []
+        pos_vector = np.zeros(14)
+        for sent in seg_doc.sentences:    
+            for word in sent.words:
+                temp_pos.append((word.text, word.upos, word.xpos, word.start_char, word.end_char))
+                if word.upos in pos_dict:
+                    pos_vector[pos_dict[word.upos]] += 1
+                else:
+                    pos_vector[pos_dict['X']] += 1
+    
+        seg_pos.append({"id": seg["id"], "seek": seg["seek"], "start": seg["start"], 
+                        "end": seg["end"], "tags": temp_pos, "vector": pos_vector})
+    
+
+    ## Speaker-Segment-wise POS Tags
+    speaker_seg_pos = []
+    for i, tupl in enumerate(zip(proc_speaker_segments, asr_dict["output_data"]["speaker_segments"])):
+        seg_doc, seg = tupl
+        temp_pos = []
+        pos_vector = np.zeros(14)
+        for sent in seg_doc.sentences:    
+            for word in sent.words:
+                temp_pos.append((word.text, word.upos, word.xpos, word.start_char, word.end_char))
+                if word.upos in pos_dict:
+                    pos_vector[pos_dict[word.upos]] += 1
+                else:
+                    pos_vector[pos_dict['X']] += 1
+    
+        speaker_seg_pos.append({"id": str(i), "start": seg["start"], "end": seg["end"], 
+                                "speaker": seg["speaker"], "tags": temp_pos, "vector": pos_vector})
+
+
+    return sent_pos, seg_pos, speaker_seg_pos
+
+
+def get_ner_tags(proc_text, proc_segments, proc_speaker_segments, asr_dict, ner_dict, event_set):
+    stanza_nes = get_stanza_ner_annotations(proc_text)
+    wikifier_nes = get_wikifier_annotations(proc_text)
+    linked_entities = link_annotations(stanza_nes, wikifier_nes)
+    linked_entities = fix_entity_types(linked_entities, event_set)
+
+    ent_map = {}
+    for ent in linked_entities:
+        ent_map[ent["text"]] = ent
+
+    ## Sent-wise ent vectors
+    sent_ent_vectors = []
+    for i, sent in enumerate(proc_text.sentences):
+        temp_nes = []
+        ner_vector = np.zeros(6)
+        for ent in sent.ents:
+            if ent.text in ent_map:
+                temp_nes.append((ent.text, ent_map[ent.text]["wd_label"], ent_map[ent.text]["type"], 
+                                 ent_map[ent.text]["wd_id"], ent.start_char, ent.end_char))
+                ner_vector[ner_dict[ent_map[ent.text]["type"]]] += 1
+
+        sent_ent_vectors.append({"id": str(i), "text": sent.text, "tags": temp_nes, "vector": ner_vector})
+
+    ## Segment-wise ent vectors
+    seg_ent_vectors = []
+    for seg_doc, seg in zip(proc_segments, asr_dict["output_data"]["segments"]):
+        temp_nes = []
+        ner_vector = np.zeros(6)
+        for sent in seg_doc.sentences:
+            for ent in sent.ents:
+                if ent.text in ent_map:
+                    temp_nes.append((ent.text, ent_map[ent.text]["wd_label"], ent_map[ent.text]["type"], 
+                                     ent_map[ent.text]["wd_id"], ent.start_char, ent.end_char))
+                    ner_vector[ner_dict[ent_map[ent.text]["type"]]] += 1
+
+        seg_ent_vectors.append({"id": seg["id"], "seek": seg["seek"], "start": seg["start"], 
+                                "end": seg["end"], "tags": temp_nes, "vector": ner_vector})
+
+    
+    ## Speaker-Segment-wise ent vectors
+    speaker_seg_ent_vectors = []
+    for i, tupl in enumerate(zip(proc_speaker_segments, asr_dict["output_data"]["speaker_segments"])):
+        seg_doc, seg = tupl
+        temp_nes = []
+        ner_vector = np.zeros(6)
+        for sent in seg_doc.sentences:
+            for ent in sent.ents:
+                if ent.text in ent_map:
+                    temp_nes.append((ent.text, ent_map[ent.text]["wd_label"], ent_map[ent.text]["type"], 
+                                     ent_map[ent.text]["wd_id"], ent.start_char, ent.end_char))
+                    ner_vector[ner_dict[ent_map[ent.text]["type"]]] += 1
+
+        speaker_seg_ent_vectors.append({"id": str(i), "start": seg["start"], "end": seg["end"], 
+                                        "speaker": seg["speaker"], "tags": temp_nes, "vector": ner_vector})
+
+
+    return sent_ent_vectors, seg_ent_vectors, speaker_seg_ent_vectors
+
     
 def perform_nlp(video_path, asr_dict, nlp, 
-                model, pos_dict, ner_dict, 
+                sen_model, pos_dict, ner_dict, 
                 event_set):
     """
     Takes ASR segments and performs NLP of text and text segments for sentiment, POS and NER
@@ -77,7 +212,7 @@ def perform_nlp(video_path, asr_dict, nlp,
         video_path (str): Full path to the video file
         asr_dict (dict): ASR output dictionary
         nlp (stanza.Pipeline): Stanza NLP pipeline
-        model (germansentiment.SentimentModel): German sentiment detection model
+        sen_model (germansentiment.SentimentModel): German sentiment detection model
         pos_dict (dict): Dictionary with pos tag label map
         ner_dict (dict): Dictionary with ner label map
         event_set (set): Set of events to be considered for NER
@@ -106,84 +241,22 @@ def perform_nlp(video_path, asr_dict, nlp,
     
     ##------------------------------------------------Sentiment Analysis------------------------------------------------##
     t1 = time.time()
-    # Sentence-wise sentiment
-    all_texts = [sent.text for sent in proc_text.sentences]
-    preds, probs = model.predict_sentiment(all_texts, output_probabilities=True)
-    sentiments = [{"id": str(i), "text": text, "pred": preds[i], "prob": [probs[i][0][1], probs[i][1][1], probs[i][2][1]]} for i, text in enumerate(all_texts)]
-    video_feat_dict["output_data"]["sentiment"]["sentence_wise"] = sentiments
-
-    # Segment-wise sentiment
-    all_texts = [text_seg["text"].strip() for text_seg in asr_dict["output_data"]["segments"]]
-    preds, probs = model.predict_sentiment(all_texts, output_probabilities=True)
-    sentiments = [{"id": seg["id"], "seek": seg["seek"], "start": seg["start"], "end": seg["end"],
-                    "pred": preds[i], "prob": [probs[i][0][1], probs[i][1][1], probs[i][2][1]]} 
-                    for i, seg in enumerate(asr_dict["output_data"]["segments"])]
-    video_feat_dict["output_data"]["sentiment"]["segment_wise"] = sentiments
-
-    # Speaker-Segment-wise sentiment
-    all_texts = [text_seg["text"].strip() for text_seg in asr_dict["output_data"]["speaker_segments"]]
-    preds, probs = model.predict_sentiment(all_texts, output_probabilities=True)
-    sentiments = [{"id": str(i), "start": seg["start"], "end": seg["end"], "speaker": seg["speaker"],
-                    "pred": preds[i], "prob": [probs[i][0][1], probs[i][1][1], probs[i][2][1]]} 
-                    for i, seg in enumerate(asr_dict["output_data"]["speaker_segments"])]
-    video_feat_dict["output_data"]["sentiment"]["speakerseg_wise"] = sentiments
+    
+    sent_sentiments, seg_sentiments, spk_sentiments = get_sentiment(sen_model, asr_dict, proc_text)
+    video_feat_dict["output_data"]["sentiment"]["sentence_wise"] = sent_sentiments
+    video_feat_dict["output_data"]["sentiment"]["segment_wise"] = seg_sentiments
+    video_feat_dict["output_data"]["sentiment"]["speakerseg_wise"] = spk_sentiments
 
     print("Sentiment Analysis Done in %.2f seconds"%(time.time()-t1))
     ##------------------------------------------------Sentiment Analysis------------------------------------------------##
 
     ##------------------------------------------------Parts of Speech------------------------------------------------##
     t2 = time.time()
-    ## Sentence-wise POS Tags
-    sent_pos = []
-    for i, sent in enumerate(proc_text.sentences):
-        temp_pos = []
-        pos_vector = np.zeros(14)
-        for word in sent.words:
-            temp_pos.append((word.text, word.upos, word.xpos, word.start_char, word.end_char))
-            if word.upos in pos_dict:
-                pos_vector[pos_dict[word.upos]] += 1
-            else:
-                pos_vector[pos_dict['X']] += 1
-    
-        sent_pos.append({"id": str(i), "text": sent.text, "tags": temp_pos, "vector": pos_vector})
 
+    sent_pos, seg_pos, speaker_seg_pos = get_pos_tags(proc_text, proc_segments, proc_speaker_segments, asr_dict, pos_dict)
     video_feat_dict["output_data"]["pos"]["sentence_wise"] = sent_pos
-
-    ## Segment-wise POS Tags
-    seg_pos = []
-    for seg_doc, seg in zip(proc_segments, asr_dict["output_data"]["segments"]):
-        temp_pos = []
-        pos_vector = np.zeros(14)
-        for sent in seg_doc.sentences:    
-            for word in sent.words:
-                temp_pos.append((word.text, word.upos, word.xpos, word.start_char, word.end_char))
-                if word.upos in pos_dict:
-                    pos_vector[pos_dict[word.upos]] += 1
-                else:
-                    pos_vector[pos_dict['X']] += 1
-    
-        seg_pos.append({"id": seg["id"], "seek": seg["seek"], "start": seg["start"], 
-                        "end": seg["end"], "tags": temp_pos, "vector": pos_vector})
-    
     video_feat_dict["output_data"]["pos"]["segment_wise"] = seg_pos
-
-    ## Speaker-Segment-wise POS Tags
-    speaker_seg_pos = []
-    for i, tupl in enumerate(zip(proc_speaker_segments, asr_dict["output_data"]["speaker_segments"])):
-        seg_doc, seg = tupl
-        temp_pos = []
-        pos_vector = np.zeros(14)
-        for sent in seg_doc.sentences:    
-            for word in sent.words:
-                temp_pos.append((word.text, word.upos, word.xpos, word.start_char, word.end_char))
-                if word.upos in pos_dict:
-                    pos_vector[pos_dict[word.upos]] += 1
-                else:
-                    pos_vector[pos_dict['X']] += 1
-    
-        speaker_seg_pos.append({"id": str(i), "start": seg["start"], "end": seg["end"], "speaker": seg["speaker"], "tags": temp_pos, "vector": pos_vector})
-
-    video_feat_dict["output_data"]["pos"]["speakerseg_wise"] = speaker_seg_pos
+    video_feat_dict["output_data"]["pos"]["speakerseg_wise"] = speaker_seg_pos    
 
     print("POS Tags Done in %.2f seconds"%(time.time()-t2))
     ##------------------------------------------------Parts of Speech------------------------------------------------##
@@ -191,60 +264,10 @@ def perform_nlp(video_path, asr_dict, nlp,
     ##------------------------------------------------Named Entity Recognition------------------------------------------------##
     t3 = time.time()
 
-    stanza_nes = get_stanza_ner_annotations(proc_text)
-    wikifier_nes = get_wikifier_annotations(proc_text)
-    linked_entities = link_annotations(stanza_nes, wikifier_nes)
-    linked_entities = fix_entity_types(linked_entities, event_set)
-
-    ent_map = {}
-    for ent in linked_entities:
-        ent_map[ent["text"]] = ent
-
-    ## Sent-wise ent vectors
-    sent_ent_vectors = []
-    for i, sent in enumerate(proc_text.sentences):
-        temp_nes = []
-        ner_vector = np.zeros(6)
-        for ent in sent.ents:
-            if ent.text in ent_map:
-                temp_nes.append((ent.text, ent_map[ent.text]["type"], ent_map[ent.text]["wd_id"], ent.start_char, ent.end_char))
-                ner_vector[ner_dict[ent_map[ent.text]["type"]]] += 1
-
-        sent_ent_vectors.append({"id": str(i), "text": sent.text, "tags": temp_nes, "vector": ner_vector})
-
+    sent_ent_vectors, seg_ent_vectors, speaker_seg_ent_vectors = get_ner_tags(proc_text, proc_segments, proc_speaker_segments, 
+                                                                              asr_dict, ner_dict, event_set)
     video_feat_dict["output_data"]["ner"]["sentence_wise"] = sent_ent_vectors
-
-    ## Segment-wise ent vectors
-    seg_ent_vectors = []
-    for seg_doc, seg in zip(proc_segments, asr_dict["output_data"]["segments"]):
-        temp_nes = []
-        ner_vector = np.zeros(6)
-        for sent in seg_doc.sentences:
-            for ent in sent.ents:
-                if ent.text in ent_map:
-                    temp_nes.append((ent.text, ent_map[ent.text]["type"], ent_map[ent.text]["wd_id"], ent.start_char, ent.end_char))
-                    ner_vector[ner_dict[ent_map[ent.text]["type"]]] += 1
-
-        seg_ent_vectors.append({"id": seg["id"], "seek": seg["seek"], "start": seg["start"], 
-                                "end": seg["end"], "tags": temp_nes, "vector": ner_vector})
-
     video_feat_dict["output_data"]["ner"]["segment_wise"] = seg_ent_vectors
-    
-    ## Speaker-Segment-wise ent vectors
-    speaker_seg_ent_vectors = []
-    for i, tupl in enumerate(zip(proc_speaker_segments, asr_dict["output_data"]["speaker_segments"])):
-        seg_doc, seg = tupl
-        temp_nes = []
-        ner_vector = np.zeros(6)
-        for sent in seg_doc.sentences:
-            for ent in sent.ents:
-                if ent.text in ent_map:
-                    temp_nes.append((ent.text, ent_map[ent.text]["type"], ent_map[ent.text]["wd_id"], ent.start_char, ent.end_char))
-                    ner_vector[ner_dict[ent_map[ent.text]["type"]]] += 1
-
-        speaker_seg_ent_vectors.append({"id": str(i), "start": seg["start"], "end": seg["end"], 
-                                        "speaker": seg["speaker"], "tags": temp_nes, "vector": ner_vector})
-
     video_feat_dict["output_data"]["ner"]["speakerseg_wise"] = speaker_seg_ent_vectors
 
     print("Named Entity Recognition Done in %.2f seconds"%(time.time()-t3))
@@ -291,7 +314,7 @@ def main():
 
             video_feat_dict = perform_nlp(video_path, asr_dict, nlp, 
                                             sen_model, pos_dict, ner_dict, event_set)
-            
+                
             with open(os.path.join(out_loc, "asr_textnlp.pkl"), "wb") as output_file:
                 pickle.dump(video_feat_dict, output_file)
 
