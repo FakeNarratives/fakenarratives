8e7212eeb20c551ba2b9e51532358c7abb032b37
gullalc <gullalcheema@gmail.com>
2023-11-08 15:10:42 +0100

    ASR and diarization update

--- audio_analysis/pickle_asr_outputs2.py
+++ audio_analysis/pickle_asr_outputs2.py
@@ -3,12 +3,9 @@ import pickle
 import tempfile
 from moviepy.editor import VideoFileClip
 import torch
-import whisper
 import argparse
-from pyannote.audio import Pipeline
 import sys
-sys.path.append("audio_analysis/pyannote-whisper/")
-from pyannote_whisper.utils import diarize_text
+import whisperx
 import yaml
 
 def parse_args():
@@ -27,18 +24,17 @@ def read_video_paths(file_path, base_input_dir, base_output_dir):
 
 
 def get_model(device, config):
-    model = whisper.load_model("large-v2")
-    model.to(device)
+    model = whisperx.load_model("large-v2", device=device, compute_type="float16")
 
-    pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization", use_auth_token=config['huggingface']['token'])
-    pipeline.to(device)
+    diarize_model = whisperx.DiarizationPipeline(use_auth_token=config['huggingface']['token'], device=device)
 
-    return model, pipeline
+    alignment_model, metadata = whisperx.load_align_model(language_code="de", device=device)
 
-def transcribe_video(script_dir, video_path, model, pipeline):
+    return model, diarize_model, alignment_model, metadata
+
+def transcribe_video(script_dir, video_path, model, diarize_model, alignment_model, metadata, device):
     """
-    Transcribes video using OpenAI's Whisper ASR model and stores details in a dictionary.
-    Also performs speaker diarization.
+    Transcribes video using WhisperX library and performs speaker diarization.
 
     The dictionary includes:
     1. GitHub repo of the Whisper model
@@ -50,7 +46,11 @@ def transcribe_video(script_dir, video_path, model, pipeline):
     Args:
         script_dir (str): Full path of this script
         video_path (str): Full path to the video file
-        model (whisper.asr.ASRModel): Initialized Whisper ASR model
+        model (whisperx model): Initialized Whisperx ASR model
+        diarize_model (whisperx model): Initialized Whisperx diarization model
+        alignment_model (whisperx model): Initialized Whisperx alignment model
+        metadata (dict): Metadata of the alignment model
+        device (str): Device to run the model on
 
     Returns:
         dict: Dictionary containing:
@@ -61,27 +61,39 @@ def transcribe_video(script_dir, video_path, model, pipeline):
             output_data (str): Transcription and diarization result from the Whisper model as a dictionary with keys: ['text', 'segments', 'language', 'speaker_segments']
     """
 
-    video_feat_dict = {"github_repo": "https://github.com/openai/whisper;https://github.com/pyannote/pyannote-audio",
-                        "commit_id": "fcfeaf1b61994c071bba62da47d7846933576ac9;28fcf502db86747bafb126720d6b95d7c8277295",
+    video_feat_dict = {"github_repo": "https://github.com/m-bain/whisperX",
+                        "commit_id": "d8a2b4ffc9e33584e453c3d433cedf6f60651350",
                         "parameters": "default",
                         "video_file": video_path,
                       }
 
     video = VideoFileClip(video_path+".mp4")
 
+    asr_result = {}
+
     ## Changed temporary audio files from .mp3 to .wav to avoid loading errors
     with tempfile.NamedTemporaryFile(dir=os.path.join(script_dir, "temp/"), suffix=".wav", delete=True) as tmp:
         video.audio.write_audiofile(tmp.name)
-        asr_result = model.transcribe(tmp.name, word_timestamps=True)
-        diarization_result = pipeline(tmp.name)
+        audio = whisperx.load_audio(tmp.name)
 
-    final_result = diarize_text(asr_result, diarization_result)
+        # result = model.transcribe(tmp.name)
+        result = model.transcribe(audio, batch_size=128, language="de")
+        text = ""
+        for seg in result["segments"]:
+            text += seg["text"].strip() + " "
+        asr_result["text"] = text.strip()
+        asr_result["segments"] = result["segments"]
 
-    speaker_segments = []
-    for seg, spk, sent in final_result:
-        speaker_segments.append({"start": seg.start, "end": seg.end, "speaker": spk, "text": sent})
+        diarize_segments = diarize_model(audio)
 
-    asr_result["speaker_segments"] = speaker_segments
+        result = whisperx.align(result["segments"], alignment_model, metadata, audio, device, return_char_alignments=False)
+
+        result = whisperx.assign_word_speakers(diarize_segments, result)
+
+    ## Save results as a dictionary: dict_keys(['text', 'segments', 'language', 'speaker_segments'])
+    asr_result["speaker_segments"] = result["segments"]
+    asr_result["language"] = "de"
+    asr_result["word_segments"] = result["word_segments"]
 
     video_feat_dict["output_data"] = asr_result
 
@@ -91,7 +103,7 @@ def transcribe_video(script_dir, video_path, model, pipeline):
 def main():
     args = parse_args()
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    device = "cuda" if torch.cuda.is_available() else "cpu"
 
     script_dir = os.path.dirname(os.path.abspath(__file__))
 
@@ -102,7 +114,7 @@ def main():
     ## File has lines with video names such as "Tagesschau/TV-20220101-2019-5100.webl.h264"
     input_paths, output_paths = read_video_paths(args.file, args.inp_dir, args.out_dir)
 
-    model, pipeline = get_model(device, config)
+    model, diarize_model, alignment_model, metadata = get_model(device, config)
 
     if not os.path.exists(os.path.join(script_dir, "temp/")):
         os.makedirs(os.path.join(script_dir, "temp/"))
@@ -115,9 +127,9 @@ def main():
         if not os.path.exists(out_loc):
             os.makedirs(out_loc)
 
-        video_feat_dict = transcribe_video(script_dir, input_path, model, pipeline)
+        video_feat_dict = transcribe_video(script_dir, input_path, model, diarize_model, alignment_model, metadata, device)
 
-        with open(os.path.join(out_loc, "asr_whisper.pkl"), "wb") as f:
+        with open(os.path.join(out_loc, "asr_whisperx.pkl"), "wb") as f:
             pickle.dump(video_feat_dict, f)
 
         print()
