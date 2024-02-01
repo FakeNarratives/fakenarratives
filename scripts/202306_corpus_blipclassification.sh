e06104a7ca8396f75217c0eb04cf48aacd8251f2
eric-mb <ericmb@tuta.io>
2024-02-01 15:12:40 +0100

    new queries added

--- scripts/202306_corpus_blipclassification.sh
+++ scripts/202306_corpus_blipclassification.sh
@@ -2,20 +2,21 @@
 #SBATCH --nodes=1
 ##SBATCH -w devbox2
 ##SBATCH -s
-#SBATCH -J BLIP_fakenarratives
+#SBATCH -J FN_BildTV
 #SBATCH -G 1
 #SBATCH --mem 32G  
 #SBATCH -c 2
 ##SBATCH -o /nfs/home/muellerer/fakenarratives/slurm
 
 FPS=1
-NEWSCHANNELS=(Tagesschau)  # (BildTV, CompactTV, HeuteJournal, Tagesschau)
+NEWSCHANNELS=(BildTV, CompactTV, HeuteJournal, Tagesschau)  # (BildTV, CompactTV, HeuteJournal, Tagesschau)
 
-QUERIES=("queries/actions.csv", "queries/news_events.csv", "queries/news_roles.csv")
+QUERIES=("queries/actions.csv" "queries/daytime.csv" "queries/environment.csv" "queries/news_events.csv" "queries/news_roles.csv "queries/weather.csv"")
 
 for c in ${NEWSCHANNELS[*]}
 do
     echo %%%% $c
+    echo ${QUERIES[*]}
     videos=$(ls -d /nfs/data/fakenarratives/202306_corpus/videos/$c/*.mp4)
-    python blip_object_classification.py --videos $videos --output /nfs/data/fakenarratives/202306_corpus/results_pkl/$c -q queries/*csv --fps $FPS
+    python blip_object_classification.py --videos $videos --output /nfs/data/fakenarratives/202306_corpus/results_pkl/$c -q ${QUERIES[*]} --fps $FPS
 done
