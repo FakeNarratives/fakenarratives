eee8729f20d69f969d5cfee54079e56999c8df65
eric-mb <ericmb@tuta.io>
2024-02-05 12:17:44 +0100

    change standard values

--- scripts/202306_corpus_blipclassification.sh
+++ scripts/202306_corpus_blipclassification.sh
@@ -2,16 +2,17 @@
 #SBATCH --nodes=1
 ##SBATCH -w devbox2
 ##SBATCH -s
-#SBATCH -J FN_BildTV
+#SBATCH -J FN_BLIP
 #SBATCH -G 1
 #SBATCH --mem 32G  
 #SBATCH -c 2
 ##SBATCH -o /nfs/home/muellerer/fakenarratives/slurm
 
 FPS=1
-NEWSCHANNELS=(BildTV, CompactTV, HeuteJournal, Tagesschau)  # (BildTV, CompactTV, HeuteJournal, Tagesschau)
+# (BildTV CompactTV HeuteJournal Tagesschau)
+NEWSCHANNELS=(BildTV CompactTV HeuteJournal Tagesschau)  
 
-QUERIES=("queries/actions.csv" "queries/daytime.csv" "queries/environment.csv" "queries/news_events.csv" "queries/news_roles.csv "queries/weather.csv"")
+QUERIES=("queries/actions.csv" "queries/daytime.csv" "queries/environment.csv" "queries/news_events.csv" "queries/news_roles.csv" "queries/weather.csv")
 
 for c in ${NEWSCHANNELS[*]}
 do
