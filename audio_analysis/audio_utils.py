d838929bab478c0759952e05511a5772830f7e42
gullalc <gullalcheema@gmail.com>
2024-05-29 01:15:41 +0200

    audio shot similarity

--- audio_analysis/audio_utils.py
+++ audio_analysis/audio_utils.py
@@ -3,6 +3,23 @@ import numpy as np
 import torch
 import random
 
+def exact_div(x, y):
+    assert x % y == 0
+    return x // y
+
+## From Whisper Code
+# hard-coded audio hyperparameters
+SAMPLE_RATE = 16000
+N_FFT = 400
+HOP_LENGTH = 160
+CHUNK_LENGTH = 30
+N_SAMPLES = CHUNK_LENGTH * SAMPLE_RATE  # 480000 samples in a 30-second chunk
+N_FRAMES = exact_div(N_SAMPLES, HOP_LENGTH)  # 3000 frames in a mel spectrogram input
+
+N_SAMPLES_PER_TOKEN = HOP_LENGTH * 2  # the initial convolutions has stride 2
+FRAMES_PER_SECOND = exact_div(SAMPLE_RATE, HOP_LENGTH)  # 10ms per audio frame
+TOKENS_PER_SECOND = exact_div(SAMPLE_RATE, N_SAMPLES_PER_TOKEN)  # 20ms per audio token
+
 # def get_speaker_turns(speaker_segments, speaker_seg_tol=3.0):
 #     for i, segment in enumerate(speaker_segments):
 #         start_time = round(segment["start"], 2)
